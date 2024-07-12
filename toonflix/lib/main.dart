375a29914b94d828ed06475b292d14457761a1fc
jhle0 <dumiso04@gmail.com>
2024-07-12 21:51:54 +0900

    #3 UI 0~4

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,22 +1,94 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
-class App extends StatelessWidget{
+class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello flutter!'),
-          backgroundColor: Color.fromRGBO(23, 134, 2, 100),
-        ),
-        body: Center(
-          child: Text('Hello world!'),
+        home: Scaffold(
+      backgroundColor: const Color(0xFF181818),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Text(
+                      'Hey, leo',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    Text(
+                      'Welcome back',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            Text(
+              'Total balance',
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.white.withOpacity(0.8),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              '\$5 194 482',
+              style: TextStyle(
+                fontSize: 48,
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(45),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                    child: Text('Transfer',
+                        style: TextStyle(
+                          fontSize: 20,
+                        )),
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
-    );
+    ));
   }
 }
