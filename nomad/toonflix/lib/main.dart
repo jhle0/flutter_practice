94394ba80e6d9213903c4e8974d17064680c9c8d
jhle0 <dumiso04@gmail.com>
2024-07-27 22:34:26 +0900

    nomad - toonflix

--- nomad/toonflix/lib/main.dart
+++ nomad/toonflix/lib/main.dart
@@ -1,18 +1,30 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
 import 'package:toonflix/services/api_service.dart';
 
-void main() {
-  ApiService().getTodaysToons();
-  runApp(const MyApp());
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..userAgent =
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() {
+  HttpOverrides.global = MyHttpOverrides();
+
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomeScreen(),
     );
   }
